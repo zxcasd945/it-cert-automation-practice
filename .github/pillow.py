fd52fcec0ed779e64904a7448974a71b3ea12115
zxcasd945 <109208358+zxcasd945@users.noreply.github.com>
2022-08-22 03:53:27 -0700

    Update pillow.py

--- .github/pillow.py
+++ .github/pillow.py
@@ -1,23 +1,3 @@
-from PIL import Image
-from glob import glob
-import os
-p='D:\Download\images\*'
-# Note: put this script in images folder
-# Iterate through each file in the folder
-for file in glob(p): # ignore hidden file (images/.DS_Store) from iteration
-    image = Image.open(file).convert('RGB')
-    # print(image.format, image.size, image.mode) # test
-    
-    path, filename = os.path.splitext(file)
-    print(path)
-     # get filename without extension
-    image.rotate(270).resize((128,128)).save('{}.jpeg'.format(path))
-
-print('OK')
-
-
-
-
 
 from PIL import Image
 from glob import glob
